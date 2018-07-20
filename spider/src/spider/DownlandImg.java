e3143001547b2052edc4418fb0b7e2f92aa1a563
bearGD <1113430218@qq.com>
2018-07-20 20:42:03 +0800

    实现了豆瓣电影排行信息的爬取
    对豆瓣电影Top250电影详细信息进行全面爬取(包括电影名、导演、编剧、主演、海报、评分等)，并且将每部电影的海报下载至本地！

---
+++ spider/src/spider/DownlandImg.java
@@ -0,0 +1,40 @@
+package spider;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+
+public class DownlandImg implements Runnable{
+
+	Film film;
+	
+	public DownlandImg(Film film) {
+		this.film = film;
+	}
+	
+	@Override
+	public void run() {
+			File path = new File("D:\\豆瓣电影IMG");
+			if(!path.exists())
+				path.mkdir();
+			
+			try (FileOutputStream fout = new FileOutputStream(new File(path,film.getId()+ ".jpg"))){
+				byte[] data = new OkHttpClient.Builder().build()
+					.newCall(new Request.Builder().url(film.getPoster()).build())
+					.execute()
+					.body()
+					.bytes();
+				fout.write(data);
+				fout.flush();
+				fout.close();
+				System.out.println(Thread.currentThread().getName() + " 下载了  " + film.getTitle() + "的海报");
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+//			System.out.println(Thread.currentThread().getName() + " is over");	
+	}
+	
+}
